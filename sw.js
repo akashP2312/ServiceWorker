c2de57b56583a8d33c95efa25e6978ea23ba59f5
Akash Patel <aspatel2312@gmail.com>
2021-08-30 19:51:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
